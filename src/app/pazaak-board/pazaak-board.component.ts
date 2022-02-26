551f0047e1d900a0f053d4b3f59e0d997c60c3eb
David Doan <daviddoan45@gmail.com>
2022-02-25 19:15:35 -0500

    implemented given number card being added to pazaak grid

--- src/app/pazaak-board/pazaak-board.component.ts
+++ src/app/pazaak-board/pazaak-board.component.ts
@@ -7,7 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PazaakBoardComponent implements OnInit {
 
+  mainDeck: Array<number> = [1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10];
+
+  testDeck: Array<number> = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  randomIndex: any;
+  randomNum: any;
+
+  playerGrid: Array<number> = [];
+  computerGrid: Array<number> = [];
+  playerTurn: boolean = true;
+
   constructor() { }
+
+  randomizeNum(max: number) {
+    return Math.floor(Math.random() * max);
+  }
+
+  startGame(): void {
+    this.randomIndex = this.randomizeNum(this.mainDeck.length)
+    this.randomNum = this.mainDeck[this.randomIndex]
+    this.playerGrid.push(this.randomNum);
+    this.mainDeck.splice(this.randomIndex, 1);
+    console.log("playerGrid: ", this.playerGrid);
+    console.log("mainDeck: ", this.mainDeck);
+  }
+
+  endGame(): void {
+    if (this.playerTurn) {
+      this.playerGrid.push(this.mainDeck[0]);
+    } else {
+      this.computerGrid.push(this.mainDeck[0]);
+    }
+  }
 
   ngOnInit(): void {
   }
