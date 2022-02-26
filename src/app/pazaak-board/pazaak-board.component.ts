8c8defb8d29fb27ede795eab0df9c1b9add18951
David Doan <daviddoan45@gmail.com>
2022-02-26 18:16:48 -0500

    changed end turn button code and implmented hide start button directive

--- src/app/pazaak-board/pazaak-board.component.ts
+++ src/app/pazaak-board/pazaak-board.component.ts
@@ -11,8 +11,10 @@ export class PazaakBoardComponent implements OnInit {
 
   testDeck: Array<number> = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-  randomIndex: any;
-  randomNum: any;
+  randomPlayerIndex: any;
+  randomPlayerNum: any;
+  randomComputerIndex: any;
+  randomComputerNum: any;
 
   playerGrid: Array<number> = [];
   computerGrid: Array<number> = [];
@@ -25,25 +27,48 @@ export class PazaakBoardComponent implements OnInit {
   }
 
   startGame(): void {
-    this.randomIndex = this.randomizeNum(this.mainDeck.length);
-    this.randomNum = this.mainDeck[this.randomIndex];
-    this.playerGrid.push(this.randomNum);
-    this.mainDeck.splice(this.randomIndex, 1);
+    this.randomPlayerIndex = this.randomizeNum(this.mainDeck.length);
+    this.randomPlayerNum = this.mainDeck[this.randomPlayerIndex];
+    this.playerGrid.push(this.randomPlayerNum);
+    this.mainDeck.splice(this.randomPlayerIndex, 1);
     this.playerTurn = false;
   }
 
-  endGame(): void {
-    this.randomIndex = this.randomizeNum(this.mainDeck.length);
-    this.randomNum = this.mainDeck[this.randomIndex];
-    if (this.playerTurn) {
-      this.playerGrid.push(this.randomNum);
-      this.mainDeck.splice(this.randomIndex, 1);
-      this.playerTurn = false;
+  endTurn(): void {
+    this.randomPlayerIndex = this.randomizeNum(this.mainDeck.length);
+    this.randomPlayerNum = this.mainDeck[this.randomPlayerIndex];
+    this.randomComputerIndex = this.randomizeNum(this.mainDeck.length);
+    this.randomComputerNum = this.mainDeck[this.randomComputerIndex];
+    // if (this.playerTurn) {
+    //   this.playerGrid.push(this.randomNum);
+    //   this.mainDeck.splice(this.randomIndex, 1);
+    //   this.playerTurn = false;
+    // } else {
+    //   this.computerGrid.push(this.randomNum);
+    //   this.mainDeck.splice(this.randomIndex, 1);
+    //   this.playerTurn = true;
+    //   this.playerGrid.push(this.randomNum);
+    //   this.mainDeck.splice(this.randomIndex, 1);
+    // }
+    if (this.playerGrid.length < 9) {
+      this.computerGrid.push(this.randomComputerNum);
+      this.mainDeck.splice(this.randomComputerIndex, 1);
+      this.playerGrid.push(this.randomPlayerNum);
+      this.mainDeck.splice(this.randomPlayerIndex, 1);
     } else {
-      this.computerGrid.push(this.randomNum);
-      this.mainDeck.splice(this.randomIndex, 1);
-      this.playerTurn = true;
+      if (this.playerGrid.length === 9) {
+        alert("Player wins!");
+        this.playerGrid = [];
+        this.computerGrid = [];
+        this.startGame();
+      }
     }
+    
+    if (this.mainDeck.length === 0) {
+      alert("No more cards, main deck has been reset.");
+      this.mainDeck = [1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10];
+    }
+    
   }
 
   ngOnInit(): void {
--- src/app/start-game.directive.ts
+++ src/app/start-game.directive.ts
@@ -1,10 +1,17 @@
-import { Directive } from '@angular/core';
+import { Directive, HostListener, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appStartGame]'
 })
 export class StartGameDirective {
 
-  constructor() { }
+  constructor(private elem : ElementRef) { }
 
+  private hideButton(display: string) {
+    this.elem.nativeElement.style.display = display;
+  }
+
+  @HostListener("click") onClick() {
+    this.hideButton('none');
+  }
 }
