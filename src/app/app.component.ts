28d53618996c3acdfa61da2f4a123cd88a91da19
David Doan <daviddoan45@gmail.com>
2022-03-01 17:21:43 -0500

    getUser request is working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './services/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'pazaak-frontend';
+
+  constructor(private userService: UserService) { }
+
+  getUser(): void {
+    this.userService.getUser().subscribe(
+      (response) => console.log(response)
+    );
+  }
+
+  ngOnInit(): void {
+      this.getUser();
+  }
 }
