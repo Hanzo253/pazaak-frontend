fb1263a2f7bd7d6a49d95da3cfd06772e5df5760
David Doan <daviddoan45@gmail.com>
2022-03-01 10:50:30 -0500

    added cantina band song to main menu and match history return to main menu button

--- src/app/main-menu/main-menu.component.ts
+++ src/app/main-menu/main-menu.component.ts
@@ -7,7 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainMenuComponent implements OnInit {
 
+  bandPlaying: any;
+  cantinaSong: any = new Audio();
+
   constructor() { }
+
+  playBand(): void {
+    this.bandPlaying = true;
+    this.cantinaSong.src = "../../assets/music/cantina-song.mp3";
+    this.cantinaSong.play();
+    this.cantinaSong.loop = true;
+  }
+
+  playGame(): void {
+    this.cantinaSong.pause();
+    this.cantinaSong.currentTime = 0;
+    this.bandPlaying = false;
+  }
+
+  matchHistory() {
+    this.cantinaSong.pause();
+    this.cantinaSong.currentTime = 0;
+    this.bandPlaying = false;
+  }
 
   ngOnInit(): void {
   }
