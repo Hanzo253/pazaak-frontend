41e966a01db6c919a510446d5aaee37c9820c16f
David Doan <daviddoan45@gmail.com>
2022-03-14 15:17:25 -0400

    designed homepage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 import { MainMenuComponent } from './main-menu/main-menu.component';
 import { MatchHistoryComponent } from './match-history/match-history.component';
 import { PazaakBoardComponent } from './pazaak-board/pazaak-board.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   {
     path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'mainmenu',
     component: MainMenuComponent
   },
   {
